ba782b7515af80e820537fedad1fe40a616cc8aa
RaulRva <111990901+RaulRva@users.noreply.github.com>
2024-05-05 21:59:06 +0000

    mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,12 +2,12 @@ import hero from  '../assets/Hero.jpg'
 function Hero() {
 
   return (
-    <div className="container rounded bg-gray-800 text-white py-20 flex items-center justify-evenly m-auto">
+    <div className="container rounded bg-gray-800 text-white py-20 md:flex items-center justify-evenly m-auto px-4 text-center">
     <div className="w-full flex justify-center">
       <img src={hero} alt="Imagen" className="w-80 h-80 object-cover rounded-full" />
     </div>
 
-    <div className='w-full'>
+    <div className='w-full mt-8 md:mt-0'>
       <h1 className="text-4xl font-bold mb-4">¡Bienvenidos a Shopify Sensei!</h1>
       <p className="text-lg">Este blog está dedicado a todos esos pequeños comercios que quieren aventurarse a dar el salto a la venta online.<br></br> En el encontrarás consejos o tips para tu tiendo online.</p>
     </div>
